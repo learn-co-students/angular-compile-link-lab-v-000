c62615fbd329238866e90e13275ed746e64b131d
github username <mendelblesofsky@gmail.com>
2016-03-28 13:50:04 -0400

    Done

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -2,11 +2,16 @@ var path = require('path');
 
 describe('Directive Test', function() {
 	browser.get('http://localhost:8080');
+	var content = element(by.css('some-directive'));
 
 	it('should change the text', function() {
-		var content = element(by.css('some-directive'));
-
 		expect(content.getText()).not.toEqual('Replace this text!');
+	});
+
+	it('should trigger a click event', function() {
+		content.click();
+ 		var alertDialog = browser.switchTo().alert();
+        expect(alertDialog.accept).toBeDefined();
 	});
 
 });
