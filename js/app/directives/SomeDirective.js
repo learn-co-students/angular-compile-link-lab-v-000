a99092f30583f73673fe9cd6531a98a3807b1724
Justin Thomas <jthomas0209@gmail.com>
2016-03-31 09:23:41 -0400

    Done

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -5,8 +5,17 @@ function SomeDirective() {
 				'Replace this text!',
 			'</div>'
 		].join(''),
-		link: function (scope, elem, attrs) {
-
+		compile: function (elem, attrs) {
+			return{
+				pre:function(scope, elem, attrs){
+					elem[0].innerText = "New Content";
+				},
+				post:function(scope, elem, attrs){
+					elem[0].addEventListener("click", function(){
+						alert("EVENT!!!!1");
+					});
+				}
+			}
 		}
 	}
 }
