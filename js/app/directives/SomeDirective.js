41736956012e1ea8a4bf249a170b9b2532ff9b23
tuckerbohman5 <tucker.bohman@gmail.com>
2016-04-01 15:24:35 -0600

    Done.

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -5,12 +5,37 @@ function SomeDirective() {
 				'Replace this text!',
 			'</div>'
 		].join(''),
-		link: function (scope, elem, attrs) {
+		compile: function (scope, elem, attrs) {
+			return {
+				pre: function(scope, element, attrs) {
+					element[0].innerText = 'New Text';
+				},
+				post: function(scope, element, attrs) {
+					element[0].addEventListener('click', function(event){
+						alert('You just clicked the elment!');
+					});
+				}
+			};
 
 		}
-	}
+	};
 }
 
 angular
 	.module('app')
 	.directive('someDirective', SomeDirective);
+
+
+
+
+
+
+	compile: function ($element, $attrs) {
+        console.log('compile');
+ 
+        return {
+            pre: function (scope, element, attrs) {
+                console.log('pre-link');
+            },
+            post: function (scope, element, attrs) {
+                console.log('post-link');
