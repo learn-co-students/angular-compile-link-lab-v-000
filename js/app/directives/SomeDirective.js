a2a687433c1d2ea2827de03dba03813da538b3fa
authorbeard <msnolan@gmail.com>
2016-05-23 19:54:34 -0400

    spec passing

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -5,6 +5,19 @@ function SomeDirective() {
 				'Replace this text!',
 			'</div>'
 		].join(''),
+		compile: function($element, $attrs){
+			return {
+				pre: function(scope, element, attrs){
+					// debugger;
+					element.text("replaced")
+				},
+
+				post: function(scope, element, attrs){
+					// debugger;
+				}
+			}
+
+		},
 		link: function (scope, elem, attrs) {
 
 		}
