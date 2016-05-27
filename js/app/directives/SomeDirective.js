36bfe117857964e6c65469e49c1101f1eb0d6412
Alvinluipo <lualvin1234@gmail.com>
2016-05-27 02:47:03 -0400

    Done.

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -5,12 +5,19 @@ function SomeDirective() {
 				'Replace this text!',
 			'</div>'
 		].join(''),
-		link: function (scope, elem, attrs) {
+		compile: function ($element, $attrs) {
 
-		}
-	}
+        return {
+            pre: function (scope, element, attrs) {
+                element[0].innerText = "This is getting harder";
+            },
+            post: function (scope, element, attrs) {
+                element[0].addEventListener;
+            }
+        }
+    }
+  };
 }
-
 angular
 	.module('app')
 	.directive('someDirective', SomeDirective);
