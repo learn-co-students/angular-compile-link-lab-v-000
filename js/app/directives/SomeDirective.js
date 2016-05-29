58fb5ca5827d45cfcc689eba60f772262ee8a046
cernanb <cernanb@gmail.com>
2016-05-28 20:11:02 -0700

    used compile and pre-link functions to manipulate dom and add event listeners

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -7,6 +7,20 @@ function SomeDirective() {
 		].join(''),
 		link: function (scope, elem, attrs) {
 
+		},
+		compile: function($element, $attrs){
+			$element[0].textContent = "New content!";
+			return {
+				pre: function(scope, element, attrs){
+					console.log('pre-link');
+				},
+				post: function(scope, element, attrs){
+					console.log(element);
+					element[0].addEventListener('click', function(){
+						alert("Hello");
+					});
+				}
+			}
 		}
 	}
 }
