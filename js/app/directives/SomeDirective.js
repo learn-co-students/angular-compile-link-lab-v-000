55dfb43bf143525d4bbdac1e3b538fadab27d67a
Tyler Smith <oats000@hotmail.com>
2016-06-25 14:50:05 +0000

    Done.

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -5,8 +5,25 @@ function SomeDirective() {
 				'Replace this text!',
 			'</div>'
 		].join(''),
+		controller: function () {
+
+      this.sendAlert = function () {
+        alert('Alert!');
+      };
+
+    },
 		link: function (scope, elem, attrs) {
 
+		},
+		compile: function ($element, $attrs) {
+				console.log('compile');
+				angular.element('div').value().replaceWith('New Text')
+				return {
+
+						post: function (scope, element, attrs) {
+								angular.element('div').append('<button class="btn btn-info" ng-click="sendAlert()">Alert</button>';)
+						}
+				}
 		}
 	}
 }
