199c041e4fbc458adf56320819cea0852da3f049
corya0687 <corya0687@gmail.com>
2016-08-29 19:32:36 -0400

    Done.

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -1,13 +1,19 @@
 function SomeDirective() {
+
 	return {
+		restrict: 'EA',
+		link: function (scope, elem, attrs) {
+			debugger;
+			alert('ran')
+		},
+		compile: function ($element, $attributes) {
+				$element[0].textContent = 'New Text';
+		},
 		template: [
 			'<div>',
 				'Replace this text!',
 			'</div>'
-		].join(''),
-		link: function (scope, elem, attrs) {
-
-		}
+		].join('')
 	}
 }
 
