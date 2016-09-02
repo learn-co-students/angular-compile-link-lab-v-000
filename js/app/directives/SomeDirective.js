a9455b7fb7d65f4b310761107d7b240e522477dc
agdavid <antonio.g.david@gmail.com>
2016-09-02 12:46:24 -0400

    add comments to highlight directive lifecycle hooks

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -6,8 +6,10 @@ function SomeDirective() {
 			'</div>'
 		].join(''),
 		compile: function(elem, attrs) {
+			//update directive DOM element text before it is loaded into DOM
 			elem.html("This is new text!");
 			return {
+				//add event listener to DOM element after loaded into DOM
 				post: function(scope, elem, attrs) {
 					elem.on('click', function() {
 						alert("You clicked me!");
@@ -15,9 +17,6 @@ function SomeDirective() {
 				}
 			}
 		}
-		// link: function (scope, elem, attrs) {
-
-		// }
 	}
 }
 
