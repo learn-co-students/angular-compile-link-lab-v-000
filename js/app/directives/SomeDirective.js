f081edc96163a417aa3a9c4853794c7b1ee7e95c
wedesignstudios <deelden@gmail.com>
2016-10-13 16:42:40 -0500

    Done

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -6,7 +6,19 @@ function SomeDirective() {
 			'</div>'
 		].join(''),
 		link: function (scope, elem, attrs) {
-
+			
+		},
+		compile: function ($element, $attrs) {
+			return {
+				pre: function() {
+					$element[0].textContent = "Some other text!";
+				},
+				post: function() {
+					$element[0].addEventListener("click", function() {
+						console.log("Clicked!");
+					});
+				}
+			}
 		}
 	}
 }
