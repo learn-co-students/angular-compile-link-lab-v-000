de91dc41e38c7374a54209a4c63eae5ee535c433
sensei100 <jenanton01@gmail.com>
2016-10-25 08:49:30 -0400

    Done.

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -5,10 +5,17 @@ function SomeDirective() {
 				'Replace this text!',
 			'</div>'
 		].join(''),
-		link: function (scope, elem, attrs) {
-
+		compile: function ($element, $attrs) {
+			$element.text('This is new text.');
+			return {
+				post: function ($scope, $element, $attrs) {
+					$element.on("click", function() {
+						alert("Hola!");
+					})
+				}
+			}
 		}
-	}
+	};
 }
 
 angular
