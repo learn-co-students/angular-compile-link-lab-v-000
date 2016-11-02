0d7fd4ccabbccb3187ae30ceae90cb26f14e475b
mmmeyers <marsi286@gmail.com>
2016-11-02 18:09:45 -0400

    Done.

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -5,8 +5,17 @@ function SomeDirective() {
 				'Replace this text!',
 			'</div>'
 		].join(''),
-		link: function (scope, elem, attrs) {
-
+		compile: function ($element, $attrs) {
+			return {
+				pre: function(scope, element, attrs) {
+					element[0].textContent = "Kittens";
+				},
+				post: function(scope, element, attrs) {
+					element.on('click', function() {
+						alert("Hey");
+					});
+				}
+			}
 		}
 	}
 }
