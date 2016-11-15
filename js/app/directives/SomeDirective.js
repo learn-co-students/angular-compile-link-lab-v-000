9c74f947c7530fceea4d8b5c20b63959a4d19afb
Mark <mystycs@gmail.com>
2016-11-15 15:23:09 -0500

    Done.

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -7,6 +7,18 @@ function SomeDirective() {
 		].join(''),
 		link: function (scope, elem, attrs) {
 
+		},
+		compile: function($element, $attrs) {
+			return {
+				pre: function(scope, element, attrs){
+						element[0].textContent = 'Hey!'
+				},
+				post: function(scope, element, attrs){
+						element[0].addEventListener('click', function(e){
+							this.text('Bye!')
+						})
+				}
+			}
 		}
 	}
 }
