5923b91d218bdb55557fdb248e5afc6e158d155b
Traci7822 <Tracir@gmail.com>
2016-11-21 15:54:35 -0700

    Done.

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -6,7 +6,18 @@ function SomeDirective() {
 			'</div>'
 		].join(''),
 		link: function (scope, elem, attrs) {
-
+		},
+		compile: function ($element, $attrs) {
+			return {
+				pre: function (scope, element, attrs) {
+					element[0].innerHTML = "<div>Things & Stuff</div>"
+				},
+				post: function (scope, element, attrs) {
+					element.on('click', function(){
+						alert('direc');
+					})
+				}
+			}
 		}
 	}
 }
