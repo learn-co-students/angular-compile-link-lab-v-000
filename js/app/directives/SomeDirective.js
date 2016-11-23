957ecf07b8e9b964459c761a1f322d9ce2c4b504
abadfish <kirsten.ofarrell@gmail.com>
2016-11-23 13:04:58 -0500

    added compile to some-directive

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -5,10 +5,19 @@ function SomeDirective() {
 				'Replace this text!',
 			'</div>'
 		].join(''),
-		link: function (scope, elem, attrs) {
-
+		compile: function(element) {
+			return {
+				pre: function(scope, element, attrs){
+					element[0].innerText = 'Changed text';
+				},
+				post: function(scope, element, attrs){
+					element[0].addEventListener('click', function() {
+						alert('Hey!');
+					});
+				}
+			}
 		}
-	}
+	};
 }
 
 angular
