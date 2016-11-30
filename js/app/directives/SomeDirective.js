a7225be9ccc9604840cb09c44bd75def76b36afe
Dom McKellar <dom.mckellar@gmail.com>
2016-11-29 17:06:31 -0800

    Done.

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -5,9 +5,20 @@ function SomeDirective() {
 				'Replace this text!',
 			'</div>'
 		].join(''),
-		link: function (scope, elem, attrs) {
-
+		compile: function(element){
+			let div = element[0];
+			return {
+				pre: function(scope, element, attrs){
+					div.innerText = "Text Replaced!";
+				},
+				post: function(scope, element, attrs){
+					div.addEventListener('click', function(){
+						alert("Div has been clicked!");
+					});
+				}
+			}
 		}
+
 	}
 }
 
