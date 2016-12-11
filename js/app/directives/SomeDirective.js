12871de44c989863e9590b464bdbf74456e5a9ec
Steve Frost <ffrostt@gmail.com>
2016-12-11 12:45:04 -0700

    used compile w/ pre & post to modify directive

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -5,10 +5,19 @@ function SomeDirective() {
 				'Replace this text!',
 			'</div>'
 		].join(''),
-		link: function (scope, elem, attrs) {
-
+		compile: function(scope, attrs) {
+			return {
+				pre: function(scope, element, attrs) {
+					element["0"].innerText = "Different text";
+				},
+				post: function(scope, element, attrs) {
+					element.on('click', function() {
+						alert('you clicked the directve');
+					});
+				}
+			};
 		}
-	}
+	};
 }
 
 angular
