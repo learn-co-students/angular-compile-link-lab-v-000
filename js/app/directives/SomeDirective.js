ca381e1cbdee7e4e23cc6799d6581840c7a4dc51
tpstar <hanlee12@gmail.com>
2017-01-04 18:15:59 -0600

    done

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -1,13 +1,25 @@
 function SomeDirective() {
 	return {
-		template: [
-			'<div>',
-				'Replace this text!',
-			'</div>'
-		].join(''),
-		link: function (scope, elem, attrs) {
+		template: `
+			<div>
+				Replace this text!
+			</div>
+		`,
+		// link: function (scope, elem, attrs) {
+		//
+		// }
+		compile: function ($element, $attrs) {
+			$element.text("Changed!!!");
+			return {
+				post: function (scope, element, attrs) {
+					console.log(scope);
+					console.log(element[0]);
+					console.log(attrs);
+					element[0].addEventListener("click", function(){element.text("Changed again!")});
+					}
 
-		}
+				}
+			}
 	}
 }
 
